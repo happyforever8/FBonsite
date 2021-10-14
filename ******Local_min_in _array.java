4efad3c6e2e3169d84a6ad1a6e11b5794605f0be
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-10-14 09:54:24 -0400

    Update ******Local_min_in _array.java

--- ******Local_min_in _array.java
+++ ******Local_min_in _array.java
@@ -65,3 +65,55 @@ Output: Index of local minima is 2
     }
 }
   
+===== find peak =============
+  
+  A peak element is an element that is strictly greater than its neighbors.
+
+Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+  
+  
+  class Solution {
+    
+// 首先要注意题目条件，在题目描述中出现了 nums[-1] = nums[n] = -∞
+// ，这就代表着 只要数组中存在一个元素比相邻元素大，那么沿着它一定可以找到一个峰值
+// 根据上述结论，我们就可以使用二分查找找到峰值
+// 查找时，左指针 l，右指针 r，以其保持左右顺序为循环条件
+// 根据左右指针计算中间位置 m，并比较 m 与 m+1 的值，如果 m 较大，则左侧存在峰值，r = m，如果 m + 1 较大，则右侧存在峰值，l = m + 1
+// 时间复杂度：O(logN)
+
+
+    public int findPeakElement(int[] nums) {
+        if (nums == null){
+            return -1;
+        }
+        if (nums.length == 0){
+            return 0;
+        }
+        
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start < end){
+            int mid = start + (end - start) / 2;
+            
+            if (nums[mid] < nums[mid + 1]){
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
